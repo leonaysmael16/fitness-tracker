aaa08703ba909481f6274c0d4942681984026451
leonaysmael16 <ysmealleona@gmail.com>
2021-12-08 22:37:17 -0800

    added routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,4 +1,29 @@
+const workout = require('../models/workout');
 const db = require('./models');
 const route = require("express").Router();
+
+// Adding total duration of workout
+route.get("/api/workout", (req, res) => {
+    db.Workout.aggregate([
+        {
+            $addFields: {
+                durationTotal: {
+                    $sum: "$duration.excercise"
+                },
+            },
+        },
+    ])
+    .then((workout) => {
+        res.json(workout);
+    });
+});
+
+route.post("/api/workout", ({ body }, res) => {
+    db.Workout.create({})
+    .then((newWorkout) => {
+        res.json(newWorkout);
+    });
+});
+
 
 module.exports = route;
